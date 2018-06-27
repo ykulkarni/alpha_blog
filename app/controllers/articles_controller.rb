2da5d5153a1093d5f552a7342c251b6d49cdcad9
ykulkarni <ykulkarni@live.com>
2018-06-26 21:34:20 -0400

    View, edit, index and show action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,9 +3,36 @@
      @article = Article.new
    end
    
+   def index
+     @articles = Article.all
+   end
+   
    def create
      @article = Article.new(article_params)
-     @article.save
+     if @article.save
+      flash[:notice] = "Article was created successfully"
+      redirect_to article_path(@article)
+     else
+       render :new
+     end
+   end
+   
+  def show
+    @article = Article.find(params[:id])
+  end
+   
+   def edit
+     @article = Article.find(params[:id])
+   end
+   
+   def update
+      @article = Article.find(params[:id])
+     if @article.update(article_params)
+       flash[:notice] = "Article was updated!"
+       redirect_to article_path(@article)
+     else
+       render :edit
+     end     
    end
    
    private 
