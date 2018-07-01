7729b2dadf93e1c70ff14f17ef4fff0ad6f027b1
Yogesh Kulkarni <ykulkarni@live.com>
2018-07-01 00:33:08 +0000

    User Model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User <ActiveRecord::Base
     
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    
+    validates :username, presence: true, 
+                uniqueness: {case_sensitive: false}, 
+                length: {minimum:4, maximum:15}
+    validates :email, presence: true, 
+                uniqueness: {case_sensitive: false}, 
+                length: {maximum: 105}, format: {with: VALID_EMAIL_REGEX}
 end
