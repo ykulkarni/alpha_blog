02b2a29dc371eee4b3df4b3159ad6bdc87309664
Yogesh Kulkarni <ykulkarni@live.com>
2018-07-01 03:06:09 +0000

    Update users

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
   get 'about' => 'pages#about'
   
   resources :articles
+  get 'signup' => 'users#new'
+  resources :users, except: [:new]
+  
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
