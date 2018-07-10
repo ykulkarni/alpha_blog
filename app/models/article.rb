daff63dfc0afdb888552c53676a668ad3f86f37d
Yogesh Kulkarni <ykulkarni@live.com>
2018-07-10 16:27:48 +0000

    Article to Category m-m association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ActiveRecord::Base
   
+  has_many :article_categories
+  has_many :categories, through: :article_categories
+  
   belongs_to :user
   validates :title, presence: true, length: {minimum: 5, maximum: 25}
   validates :description, presence: true, length: {minimum:10, maximum: 200}
